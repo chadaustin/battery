0a0b53397a860706b54f4e5024cd3667e25d2ad4
Chad Austin <chad@chadaustin.me>
2015-10-06 08:51:43 -0700

    basic test running ability

--- examples/basic/BasicTest.hs
+++ examples/basic/BasicTest.hs
@@ -1,6 +1,8 @@
+{-# LANGUAGE TemplateHaskell #-}
+
 import Battery.Test
 
 test_foo = do
     assertEqual 1 2
 
-$(testMain)
+$testMain
---
+++ src/Battery/Test.hs
@@ -0,0 +1,47 @@
+{-# LANGUAGE TemplateHaskell #-}
+
+module Battery.Test where
+
+import Language.Haskell.Extract
+import Language.Haskell.TH
+import Control.Exception (throwIO, catch, Exception)
+import Control.Monad (when, forM_)
+
+type Reason = String
+data AssertionFailed = AssertionFailed Reason
+    deriving (Show)
+
+instance Exception AssertionFailed
+
+assertionFailed :: Reason -> IO ()
+assertionFailed = throwIO . AssertionFailed
+
+assertEqual :: (Show a, Eq a) => a -> a -> IO ()
+assertEqual expected actual = do
+    when (expected /= actual) $ do
+        assertionFailed $ "expected " ++ show expected ++ " did not equal actual " ++ show actual
+
+data TestCase = TestCase String (IO ())
+
+defaultMain :: [TestCase] -> IO ()
+defaultMain tests = do
+    forM_ tests $ \(TestCase name action) -> do
+        putStrLn $ "running " ++ name
+        catch action $ \(AssertionFailed reason) -> do
+            putStrLn $ "failed " ++ name ++ " because " ++ reason
+
+testCase :: String -> IO () -> TestCase
+testCase = TestCase
+
+testMain :: Q [Dec]
+testMain = do
+    body <- [| defaultMain $(collectTests) |]
+    return [FunD (mkName "main")
+        [ Clause [] (NormalB body) []
+        ]]
+
+listGenerator :: String -> ExpQ
+listGenerator beginning = functionExtractorMap beginning (return $ VarE $ mkName "testCase")
+
+collectTests :: ExpQ
+collectTests = listGenerator "^test_"
