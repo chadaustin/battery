80303d40e91821eaf084a2ff932fb8daffd26e12
Chad Austin <chad@chadaustin.me>
2015-10-06 17:48:25 -0700

    colorize test failures

--- src/Battery/Test.hs
+++ src/Battery/Test.hs
@@ -1,4 +1,4 @@
-{-# LANGUAGE TemplateHaskell #-}
+{-# LANGUAGE TemplateHaskell, ExistentialQuantification #-}
 
 module Battery.Test where
 
@@ -6,8 +6,16 @@ import Language.Haskell.Extract
 import Language.Haskell.TH
 import Control.Exception (throwIO, catch, Exception)
 import Control.Monad (when, forM_)
+import System.Console.ANSI
 
-type Reason = String
+data Reason = forall a. Show a => NotEqual a a
+
+instance Show Reason where
+    show (NotEqual expected actual) = "NotEqual " ++ show expected ++ " " ++ show actual
+
+reasonString :: (String -> String) -> Reason -> String
+reasonString f (NotEqual expected actual) = "expected " ++ f (show expected) ++ " but got " ++ f (show actual)
+
 data AssertionFailed = AssertionFailed Reason
     deriving (Show)
 
@@ -19,21 +27,30 @@ assertionFailed = throwIO . AssertionFailed
 assertEqual :: (Show a, Eq a) => a -> a -> IO ()
 assertEqual expected actual = do
     when (expected /= actual) $ do
-        assertionFailed $ "expected " ++ show expected ++ " did not equal actual " ++ show actual
+        assertionFailed $ NotEqual expected actual
 
 type TestName = String
 data TestCase = TestCase TestName (IO ())
 
+yellow :: String -> String
+yellow s = setSGRCode [SetColor Foreground Vivid Yellow] ++ s ++ setSGRCode []
+
+red :: String -> String
+red s = setSGRCode [SetColor Foreground Vivid Red] ++ s ++ setSGRCode []
+
+green :: String -> String
+green s = setSGRCode [SetColor Foreground Vivid Green] ++ s ++ setSGRCode []
+
 recordFailure :: TestName -> Reason -> IO ()
 recordFailure name reason = do
-    putStrLn $ name ++ " failed: " ++ reason
+    putStrLn $ yellow name ++ " " ++ red "FAILED" ++ ": " ++ reasonString green reason
 
 defaultMain :: [TestCase] -> IO ()
 defaultMain tests = do
     forM_ tests $ \(TestCase name action) -> do
         putStrLn $ "running " ++ name
         catch action $ \(AssertionFailed reason) -> do
-            putStrLn $ name ++ " failed: " ++ reason
+            recordFailure name reason
 
 testCase :: String -> IO () -> TestCase
 testCase = TestCase
